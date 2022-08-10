98b02328da1528616f265adb2f66848ba9f1ef82
John Kibirige <kibirigejohn005@gmail.com>
2022-08-11 00:05:38 +0300

    Create a tasks component for rendering a given task to the UI

---
+++ modules/tasks.js
@@ -0,0 +1,44 @@
+const tasks = [
+  {
+    description: 'Read an article',
+    completed: false,
+    id: 0,
+  },
+  {
+    description: 'Watch Andrew Wommack',
+    completed: false,
+    id: 1,
+  },
+  {
+    description: 'Meditate',
+    completed: false,
+    id: 2,
+  },
+  {
+    description: 'learn tailwindCss',
+    completed: false,
+    id: 3,
+  },
+  {
+    description: 'watch a movie',
+    completed: false,
+    id: 4,
+  },
+  {
+    description: 'Be on twitter',
+    completed: false,
+    id: 5,
+  },
+  {
+    description: 'Do exercise',
+    completed: false,
+    id: 6,
+  },
+  {
+    description: 'Read the Bible',
+    completed: false,
+    id: 7,
+  },
+];
+
+export default tasks;
