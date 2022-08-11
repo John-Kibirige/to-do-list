c020745aa380321caa512090c9222586d7459e8b
John Kibirige <kibirigejohn005@gmail.com>
2022-08-11 06:41:59 +0300

    Correct the array in tasks.js to be 1 based indexed instead of zero based

--- src/modules/tasks.js
+++ src/modules/tasks.js
@@ -2,42 +2,42 @@ const tasks = [
   {
     description: 'Read an article',
     completed: false,
-    id: 0,
+    id: 1,
   },
   {
     description: 'Watch Andrew Wommack',
     completed: false,
-    id: 1,
+    id: 2,
   },
   {
     description: 'Meditate',
     completed: false,
-    id: 2,
+    id: 3,
   },
   {
     description: 'learn tailwindCss',
     completed: false,
-    id: 3,
+    id: 4,
   },
   {
     description: 'watch a movie',
     completed: false,
-    id: 4,
+    id: 5,
   },
   {
     description: 'Be on twitter',
     completed: false,
-    id: 5,
+    id: 6,
   },
   {
     description: 'Do exercise',
     completed: false,
-    id: 6,
+    id: 7,
   },
   {
     description: 'Read the Bible',
     completed: false,
-    id: 7,
+    id: 8,
   },
 ];
 
