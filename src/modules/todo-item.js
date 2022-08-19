59aaa68dd24fbe78bfef6192670c535eaf201d28
John Kibirige <kibirigejohn005@gmail.com>
2022-08-19 15:43:06 +0300

    make changes in the todo item file

--- src/modules/todo-item.js
+++ src/modules/todo-item.js
@@ -4,7 +4,6 @@ const createTodoItem = (id, description) => {
   const li = document.createElement('li');
   li.classList.add('list-item');
   li.id = `list${id}`;
-
   li.innerHTML = `
        <input type="checkbox" id="${id}" class="checkbox" />
        <label for="#${id}" id="task${id}">${description}</label>
